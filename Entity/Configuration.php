2772443aa3fcc75aade9eba0cf68886fbf4f9430
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2012-05-11 14:55:20 +0200

    created configuration entity

---
+++ Entity/Configuration.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Eotvos\VersenyrBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Represents a round of a section.
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Configuration
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string $value
+     *
+     * @ORM\Column(name="value", type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+}
